588d27dcf2b3e9cee8eb0ad93639fb9a3c030b3a
MikaelNordberg <mikael.nordberg@scb.se>
2021-01-18 09:58:11 +0100

    Updated assembly version number

--- PXWeb/Properties/AssemblyInfo.cs
+++ PXWeb/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("19.0.0.0")]
-[assembly: AssemblyFileVersion("19.0.0.0")]
+[assembly: AssemblyVersion("20.2.0.0")]
+[assembly: AssemblyFileVersion("20.2.0.0")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
