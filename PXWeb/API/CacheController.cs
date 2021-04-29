55deb4761e637c265df0d9c3bcf14973769a105a
Miska Makkonen <miska.makkonen@tilastokeskus.fi>
2021-04-29 08:52:16 +0300

    API route to contain amdin nad version sections.

--- PXWeb/API/CacheController.cs
+++ PXWeb/API/CacheController.cs
@@ -2,14 +2,17 @@
 
 namespace PXWeb.API
 {
+    /// <summary>
+    /// Cotroller for PxWeb cache manipulation
+    /// </summary>
     public class CacheController : ApiController
     {
         /// <summary>
         /// Method to delete specific cache item
         /// </summary>
         /// <param name="itemKey">Key of the cache item to be deleted</param>
-        [HttpDelete, Route("api/cache")]
-        public void Delete(string key)
+        [HttpDelete]
+        public void Delete([FromUri]string key)
         {
             // TODO: Authentication
 
