dd213e87a97807e74a7482e6d3d4e476a9d55e8a
jfi <jfi@ssb.no>
2021-05-07 17:41:03 +0200

    [PxWeb/issues/124] Changed bat so it runs in build Event

--- GetConfig.bat
+++ GetConfig.bat
@@ -1,9 +1,14 @@
 @echo off
-if exist PXWeb\Web.config (
+set mypath=%~dp0
+echo mypath %mypath%
+
+
+echo cheching if %mypath%PXWeb\Web.config exists
+if exist %mypath%PXWeb\Web.config (
   echo "Web.config already exist, so no copying."
 ) else (
   echo "Copying files:"
-  copy TemplateConfigFiles\*.config PXWeb\.
+ copy %mypath%TemplateConfigFiles\*.config %mypath%PXWeb\.
 ) 
 
 echo "Done."
