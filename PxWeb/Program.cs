b761885a169c86b3a7e19368db87aba207dd0517
cklinton <60175636+cklinton@users.noreply.github.com>
2022-05-13 15:10:24 +0200

    Added nuget AspNetCoreRateLimit for limiting number of requests per time unit.

--- PxWeb/Program.cs
+++ PxWeb/Program.cs
@@ -1,3 +1,4 @@
+using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,6 +15,25 @@ namespace PxWeb
 
             // Add services to the container.
 
+            // needed to load configuration from appsettings.json
+            builder.Services.AddOptions();
+
+            // needed to store rate limit counters and ip rules
+            builder.Services.AddMemoryCache();
+
+            //load general configuration from appsettings.json
+            builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+
+            //load ip rules from appsettings.json
+            builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+
+            // inject counter and rules stores
+            builder.Services.AddInMemoryRateLimiting();
+            
+
+            // configuration (resolvers, counter key builders)
+            builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
             // Add configuration
             builder.Services.Configure<PxApiConfigurationOptions>(builder.Configuration.GetSection("PxApiConfiguration"));
             builder.Services.AddTransient<IPxApiConfigurationService, PxApiConfigurationService>();
@@ -26,17 +46,19 @@ namespace PxWeb
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
             app.MapControllers();
+
+            app.UseIpRateLimiting();
 
             app.Run();
         }
