321a8b2e2ad9446698e5b1648d6f599d7b081ae6
MikaelNordberg <mikael.nordberg@scb.se>
2022-06-03 10:51:27 +0200

    Px.Abstractions project added

---
+++ Px.Abstractions/DataSource/CnmmDataSource.cs
@@ -0,0 +1,23 @@
+﻿using PCAxis.Menu;
+using PCAxis.Menu.Implementations;
+using Px.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Px.Abstractions.DataSource
+{
+    public class CnmmDataSource : IDataSource
+    {
+        public PxMenuBase CreateMenu(string id, string language)
+        {
+            //Create database object to return
+            DatamodelMenu retMenu = ConfigDatamodelMenu.Create(language);
+            retMenu.RootItem.Sort();
+
+            return retMenu;
+        }
+    }
+}
---
+++ Px.Abstractions/Interfaces/IDataSource.cs
@@ -0,0 +1,33 @@
+﻿using PCAxis.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Px.Abstractions.Interfaces
+{
+    public interface IDataSource
+    {
+
+        /// <summary>
+        /// Get Menu
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        PxMenuBase CreateMenu(string id, string language);
+
+        ///// <summary>
+        ///// Create builder
+        ///// </summary>
+        ///// <param name="dbi">Database</param>
+        ///// <param name="menu">Menu</param>
+        ///// <param name="selection">Selection</param>
+        ///// <param name="language">Language</param>
+        ///// <returns></returns>
+        //IPXModelBuilder CreateBuilder(IDatabaseInfo dbi, string menu, string selection, string language);
+
+        //string GetSource(IDatabaseInfo dbi, PCAxis.Paxiom.PXModel model, string language);
+    }
+}
