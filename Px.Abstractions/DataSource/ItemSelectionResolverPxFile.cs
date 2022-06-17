39ddc276127fe686d9b98f815fcf54c857aaea69
Kent Mossbäck <kent.mossback@scb.se>
2022-06-17 09:50:08 +0200

    Added itemselectionresolver for pxfile

---
+++ Px.Abstractions/DataSource/ItemSelectionResolverPxFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCAxis.Menu;
+using Px.Abstractions.Interfaces;
+
+namespace Px.Abstractions.DataSource
+{
+    public class ItemSelectionResolverPxFile : IItemSelectionResolver
+    {
+        public ItemSelection Resolve(string selection)
+        {
+            //todo: implement
+            return new ItemSelection();
+        }
+    }
+}
