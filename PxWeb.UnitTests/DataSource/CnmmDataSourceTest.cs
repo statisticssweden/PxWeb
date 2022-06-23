316089870539ddfa2ebf046d367cf42e21a68673
Kent Mossbäck <kent.mossback@scb.se>
2022-06-23 15:36:05 +0200

    Added and modified tests. Added testfactory class

--- PxWeb.UnitTests/DataSource/CnmmDataSourceTest.cs
+++ PxWeb.UnitTests/DataSource/CnmmDataSourceTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.Extensions.Caching.Memory;
+﻿using Microsoft.Extensions.Caching.Memory;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using PxWeb.Code.Api2.DataSource.Cnmm;
@@ -10,7 +9,7 @@ namespace PxWeb.UnitTests.DataSource
     public class CnmmDataSourceTest
     {
         [TestMethod]
-        public void ShouldResolveItemCollection()
+        public void ResolveShouldResolveItemCollection()
         {
             var memorymock = new Mock<IMemoryCache>();
             var entryMock = new Mock<ICacheEntry>();
@@ -18,23 +17,43 @@ namespace PxWeb.UnitTests.DataSource
             
             var pcAxisFactory = new Mock<IItemSelectionResolverFactory>();
 
-            var dict = new Dictionary<string, string>();
-
-            dict.Add("1","Ett");
-            dict.Add("2","Två");
-            dict.Add("3","Tre");
-            dict.Add("4","Fyra");
-            dict.Add("5","Fem");
-            dict.Add("6","Sex");
-
+            var testFactory = new TestFactory();
+            var dict = testFactory.GetMenuLookup();
+            
             pcAxisFactory.Setup(x => x.GetMenuLookup()).Returns(dict);
             
             var resolver = new ItemSelectionResolverCnmm(memorymock.Object, pcAxisFactory.Object);
             
-            var result = resolver.Resolve("1");
+            var result = resolver.Resolve("AA0003");
 
             Assert.IsNotNull(result);
+            Assert.AreEqual("AA", result.Menu);
         }
+
+
+        [TestMethod]
+        public void ResolveEmtySelectionItemShouldReturnStart()
+        {
+            var memorymock = new Mock<IMemoryCache>();
+            var entryMock = new Mock<ICacheEntry>();
+            memorymock.Setup(m => m.CreateEntry(It.IsAny<object>())).Returns(entryMock.Object);
+
+            var pcAxisFactory = new Mock<IItemSelectionResolverFactory>();
+
+            var testFactory = new TestFactory();
+            var dict = testFactory.GetMenuLookup();
+
+
+            pcAxisFactory.Setup(x => x.GetMenuLookup()).Returns(dict);
+
+            var resolver = new ItemSelectionResolverCnmm(memorymock.Object, pcAxisFactory.Object);
+
+            var result = resolver.Resolve("");
+
+            Assert.AreEqual("START",result.Menu );
+            Assert.AreEqual("START", result.Selection);
+        }
+
 
 
         [TestMethod]
