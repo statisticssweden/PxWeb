4cdd18125af534170441b2adf3579c9af26599da
petroslikidis <petros.likidis@gmail.com>
2022-09-13 12:32:07 +0200

    Added Lucene implementation stubs

---
+++ Px.Search.Lucene/LuceneSearcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Px.Search.Lucene
+{
+    public class LuceneSearcher : ISearcher
+    {
+        public IEnumerable<SearchResult> Find(string searchExpression, string language)
+        {
+            //Search the right index depending on the language and give back a search result.
+            // See https://github.com/statisticssweden/Px.Search.Lucene/blob/main/Px.Search.Lucene/LuceneSearcher.cs
+            throw new NotImplementedException();
+        }
+    }
+}
