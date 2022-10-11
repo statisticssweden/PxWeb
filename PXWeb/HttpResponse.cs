9e562dbe41bda22a30a0038e15f0edb99e8f6fe7
Erikson Jonathan IT/UTV/KOM-Ö <jonatan.erikson@gmail.com>
2022-10-11 14:57:54 +0200

    Add HTTP response class

---
+++ PXWeb/HttpResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PxWeb
+{
+    public class HttpResponse
+    {
+        public string content { get; set; }
+        public string contentType { get; set; }
+        public HttpResponse(string content, string responseType)
+        {
+            this.content = content;
+            this.contentType = responseType;
+        }
+    }
+}
