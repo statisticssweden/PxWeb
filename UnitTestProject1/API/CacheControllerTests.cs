f8e6d27a089ba96de71b71b5f79f24a53877fb8b
Kent Mossbäck <kent.mossback@scb.se>
2022-10-13 10:08:00 +0200

    Updated nugets for PcAxis.cora and serializers

---
+++ UnitTestProject1/API/CacheControllerTests.cs
@@ -0,0 +1,94 @@
+using Moq;
+using NUnit.Framework;
+using PXWeb.API;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//using System.Web.Http.Controllers;
+
+namespace PxWeb.Test
+{
+    public class CacheControllerTests
+    {
+
+        private Mock<PXWeb.API.Services.ICacheService> _cacheServiceMock;
+        private Mock<log4net.ILog> _logger;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Environment.SetEnvironmentVariable("APIKey", "foo");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _cacheServiceMock = new Mock<PXWeb.API.Services.ICacheService>();
+            _logger = new Mock<log4net.ILog>();
+        }
+
+        private CacheController InitializeCacheController()
+        {
+            var controller =  new CacheController(_cacheServiceMock.Object, _logger.Object)
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+
+            controller.ControllerContext.Request.Headers.Add("APIKey", "foo");
+
+            return controller;
+        }
+
+        [Test]
+        public void Delete_succeeds_ReturnsHttpStatusCode204()
+        {
+            // Arrange
+            var controller = InitializeCacheController();
+
+            // Act
+            var response = controller.Delete();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+        }
+
+        [Test]
+        public void Delete_DependenciesFail_ReturnsHttpStatusCode500()
+        {
+            // Arrange
+            _cacheServiceMock.Setup(c => c.ClearCache()).Throws<Exception>();
+            CacheController controller = InitializeCacheController();
+
+            // Act
+            var response = controller.Delete();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.InternalServerError, response.StatusCode);
+
+        }
+
+        [Test]
+        public void Delete_WithAllowedCacheType_CallsCacheService()
+        {
+            // Arrange
+            CacheController controller = InitializeCacheController();
+
+            // Act
+            var response = controller.Delete();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+            _cacheServiceMock.Verify(mock => mock.ClearCache(), Times.Once());
+
+        }
+
+        // TODO: Test unauthorized
+        // 
+        
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using PXWeb.API;
+using System.Net;
+using log4net;
+using Moq;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private Mock<PXWeb.API.Services.ICacheService> _cacheServiceMock;
+        private Mock<ILog> _logger;
+
+        [ClassInitialize]
+        public void OneTimeSetUp()
+        {
+            Environment.SetEnvironmentVariable("APIKey", "foo");
+        }
+        
+        private HttpActionContext InitializeHttpActionContext(string apiKey, bool isApiKey = true)
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            if (isApiKey)
+                request.Headers.Add("APIKey", apiKey);
+            HttpControllerContext controllerContext = new HttpControllerContext()
+            {
+                Request = request
+            };
+            HttpActionContext context = new HttpActionContext()
+            {
+                ControllerContext = controllerContext
+            };
+            return context;
+        }
+
+        [TestMethod]
+        public void Call_AuthenticationFilterWithApikey_ReturnsNull()
+        {
+            // Arrange
+            var filter = new AuthenticationFilter();
+
+            var context = InitializeHttpActionContext("foo");
+
+            // Act
+            filter.OnActionExecuting(context);
+
+            var response = context.Response;
+
+            // Assert
+            Assert.IsNull(response);
+
+        }
+
+        [DataTestMethod]
+        //[DataRow("tttt", "")]
+        [DataRow("", false)]
+        [DataRow("")]
+        [DataRow("xyz")]
+        public void Call_AuthenticationFilterWithoutApikey_ReturnsForbidden(string apiKey, bool isApiKey = true)
+        {
+            // Arrange
+            var filter = new AuthenticationFilter();
+            var context = InitializeHttpActionContext(apiKey, isApiKey);
+
+            // Act
+            filter.OnActionExecuting(context);
+
+            var response = context.Response;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+        
+        [TestMethod]
+        public void Initialize_AuthenticationFilter_WithoutEnv_ThrowsArgumentException()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable("APIKey", "");
+
+            // Act
+            try
+            {
+                var filter = new AuthenticationFilter();
+                Assert.Fail();
+            }
+            catch (ArgumentException)
+            {
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+            // Assert
+        }
+    }
+}
