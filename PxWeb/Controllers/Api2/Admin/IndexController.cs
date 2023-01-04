bada4c9e4c3b887b72862f2d5a5f9ea6d1fc54e9
MikaelNordberg <mikael.nordberg@scb.se>
2023-01-04 10:13:27 +0100

    Now with admin controller to index database

---
+++ PxWeb/Controllers/Api2/Admin/IndexController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PxWeb.Api2.Server.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace PxWeb.Controllers.Api2.Admin
+{
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/api/v2/admin/index")]
+        [SwaggerOperation("IndexDatabase")]
+        [SwaggerResponse(statusCode: 200, description: "Success")]
+        [SwaggerResponse(statusCode: 401, description: "Unauthorized")]
+        public IActionResult IndexDatabase()
+        {
+            return Ok();
+        }
+    }
+}
