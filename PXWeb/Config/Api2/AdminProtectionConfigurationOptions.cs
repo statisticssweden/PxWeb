46b49f1024759c6c488c7a3ff9eb23db97ff1721
Jonathan Erikson <jonatan.erikson@gmail.com>
2023-01-26 12:04:28 +0100

    Add adminKey to options

--- PXWeb/Config/Api2/AdminProtectionConfigurationOptions.cs
+++ PXWeb/Config/Api2/AdminProtectionConfigurationOptions.cs
@@ -5,5 +5,6 @@ namespace PxWeb.Config.Api2
     public class AdminProtectionConfigurationOptions
     {
         public List<string> IpWhitelist { get; set; } = new List<string>();
+        public string AdminKey { get; set; } = string.Empty;
     }
 }
