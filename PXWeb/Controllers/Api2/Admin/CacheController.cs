c83ef3a407005861346cda1a2b8c64cdf8850c13
Jonathan Erikson <jonatan.erikson@gmail.com>
2023-02-14 10:21:49 +0100

    Create Api endpoint that clears IPxCache singleton

---
+++ PXWeb/Controllers/Api2/Admin/CacheController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PxWeb.Code.Api2.Cache;
+
+namespace PxWeb.Controllers.Api2.Admin
+{
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IPxCache _pxCache;
+
+        public CacheController(IPxCache pxCache)
+        {
+            _pxCache = pxCache;
+        }
+
+        [HttpDelete]
+        [Route("/api/v2/admin/cache")]
+        public IActionResult Clear()
+        {
+            _pxCache.Clear();
+            return Ok();
+        }
+    }
+}
