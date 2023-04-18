be782a79dd950e0c20495c729c6fa9f9b6771e4f
MikaelNordberg <mikael.nordberg@scb.se>
2023-04-18 15:31:09 +0200

    Implemented algorithm for default selection

--- PxWeb/Code/Api2/DataSelection/SelectionHandler.cs
+++ PxWeb/Code/Api2/DataSelection/SelectionHandler.cs
@@ -21,7 +21,7 @@ namespace PxWeb.Code.Api2.DataSelection
             }
             else
             {
-                return GetDefaultTable(model);
+                return GetDefaultSelection(model);
             }
 
         }
@@ -38,28 +38,13 @@ namespace PxWeb.Code.Api2.DataSelection
             return true;
         }
 
-        //private Selection[] GetDefaultTable(PXModel model)
-        //{
-        //    //TODO implement the correct algorithm
-
-        //    var selections = new List<Selection>();
-
-        //    foreach (var variable in model.Meta.Variables)
-        //    {
-        //        var selection = new Selection(variable.Code);
-        //        //Takes the first 4 values for each variable if variable has less values it takes all of its values.
-        //        var codes = variable.Values.Take(4).Select(value => value.Code).ToArray();
-        //        selection.ValueCodes.AddRange(codes);
-        //        selections.Add(selection);
-        //    }
-
-        //    return selections.ToArray();
-        //}
-
-        private Selection[] GetDefaultTable(PXModel model)
+        /// <summary>
+        /// Get the default selection based on an algorithm
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private Selection[] GetDefaultSelection(PXModel model)
         {
-            var selections = new List<Selection>();
-
             // Only select from variables that cannot be eliminated
             var variablesWithSelection = model.Meta.Variables.FindAll(v => v.Elimination == false);
 
@@ -72,63 +57,207 @@ namespace PxWeb.Code.Api2.DataSelection
             // 2 variables - Time and Content
             if (variablesWithSelection.Count == 2 && timeVar != null && contentVar != null)
             {
-                foreach (var variable in model.Meta.Variables)
-                {
-                    var selection = new Selection(variable.Code);
-
-                    if (variable.IsContentVariable)
-                    {
-                        // Content - Takes all values
-                        var codes = contentVar.Values.Take(contentVar.Values.Count).Select(value => value.Code).ToArray();
-                        selection.ValueCodes.AddRange(codes);
-                    }
-                    else if (variable.IsTime)
-                    {
-                        // Time - Take the 12 last values
-                        var lstCodes = timeVar.Values.TakeLast(12).Select(value => value.Code).ToList();
-                        lstCodes.Sort((a, b) => b.CompareTo(a)); // Descending sort
-                        var codes = lstCodes.ToArray();
-                        selection.ValueCodes.AddRange(codes);
-                    }
-                    else
-                    {
-                        // Select nothing for the other variables
-                    }
-
-                    selections.Add(selection);
-                }
-
-                //// Content - Takes all values
-                //var selection = new Selection(contentVar.Code);
-                //var codes = contentVar.Values.Take(contentVar.Values.Count).Select(value => value.Code).ToArray();
-                //selection.ValueCodes.AddRange(codes);
-                //selections.Add(selection);
-
-                //// Time - Take the 12 last values
-                //selection = new Selection(timeVar.Code);
-                //var lstCodes = timeVar.Values.TakeLast(12).Select(value => value.Code).ToList();
-                //lstCodes.Sort((a,b) => b.CompareTo(a)); // Descending sort
-                //codes = lstCodes.ToArray();
-                //selection.ValueCodes.AddRange(codes);
-                //selections.Add(selection);
+                return GetDefaultSelectionOnlyTimeAndContent(model);
             }
-            // 3 variables - Time, Content (with only 1 value) and one more
+            // 3 variables - Time, Content (with only 1 value) and one more variable
             else if (variablesWithSelection.Count == 3 && timeVar != null && contentVar != null && contentVar.Values.Count == 1)
             {
-
+                return GetDefaultSelectionThreeVariablesTimeAndContentOneValue(model);
             }
-            // 3 variables - Time, Content (with more than 1 value) and one more
+            // 3 variables - Time, Content (with more than 1 value) and one more variable
             else if (variablesWithSelection.Count == 3 && timeVar != null && contentVar != null && contentVar.Values.Count > 1)
             {
-
+                return GetDefaultSelectionThreeVariablesTimeAndContentMoreThanOneValue(model);
             }
             // All other cases
             else
             {
+                return GetDefaultSelectionAllOtherCases(model);
+            }
+        }
 
+        /// <summary>
+        /// Returns default selection when only the time- and contentvariable cannot be eliminated
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private Selection[] GetDefaultSelectionOnlyTimeAndContent(PXModel model)
+        {
+            var selections = new List<Selection>();
+
+            foreach (var variable in model.Meta.Variables)
+            {
+                var selection = new Selection(variable.Code);
+
+                if (variable.IsContentVariable)
+                {
+                    // Content - Takes all values
+                    selection.ValueCodes.AddRange(GetAllCodes(variable));
+                }
+                else if (variable.IsTime)
+                {
+                    // Time - Take the 12 last values
+                    selection.ValueCodes.AddRange(GetTimeCodes(variable, 12));
+                }
+                else
+                {
+                    // Select nothing for the other variables
+                }
+
+                selections.Add(selection);
             }
 
             return selections.ToArray();
+        }
+
+        /// <summary>
+        /// Returns default selection when the time-, the content variable and one more variable cannot be eliminated.
+        /// Also the content variable only has one value.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private Selection[] GetDefaultSelectionThreeVariablesTimeAndContentOneValue(PXModel model)
+        {
+            var selections = new List<Selection>();
+
+            foreach (var variable in model.Meta.Variables)
+            {
+                var selection = new Selection(variable.Code);
+
+                if (variable.IsContentVariable)
+                {
+                    // Content - Take the one value
+                    selection.ValueCodes.AddRange(GetCodes(variable, 1));
+                }
+                else if (variable.IsTime)
+                {
+                    // Time - Take the 12 last values
+                    selection.ValueCodes.AddRange(GetTimeCodes(variable, 12));
+                }
+                else if (!variable.Elimination)
+                {
+                    // Take all values for the third variable
+                    selection.ValueCodes.AddRange(GetAllCodes(variable));
+                }
+                else
+                {
+                    // Select nothing for the other variables
+                }
+
+                selections.Add(selection);
+            }
+
+            return selections.ToArray();
+        }
+
+        /// <summary>
+        /// Returns default selection when the time-, the content variable and one more variable cannot be eliminated.
+        /// Also the content variable only has more than one value.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private Selection[] GetDefaultSelectionThreeVariablesTimeAndContentMoreThanOneValue(PXModel model)
+        {
+            var selections = new List<Selection>();
+
+            foreach (var variable in model.Meta.Variables)
+            {
+                var selection = new Selection(variable.Code);
+
+                if (variable.IsContentVariable)
+                {
+                    // Content - Take all values
+                    selection.ValueCodes.AddRange(GetAllCodes(variable));
+                }
+                else if (variable.IsTime)
+                {
+                    // Time - Take the last value
+                    selection.ValueCodes.AddRange(GetTimeCodes(variable, 1));
+                }
+                else if (!variable.Elimination)
+                {
+                    // Take all values for the third variable
+                    selection.ValueCodes.AddRange(GetAllCodes(variable));
+                }
+                else
+                {
+                    // Select nothing for the other variables
+                }
+
+                selections.Add(selection);
+            }
+
+            return selections.ToArray();
+        }
+
+        /// <summary>
+        /// Returns default selection for all other cases
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private Selection[] GetDefaultSelectionAllOtherCases(PXModel model)
+        {
+            var selections = new List<Selection>();
+            int variableNumber = 1;
+
+            foreach (var variable in model.Meta.Variables)
+            {
+                var selection = new Selection(variable.Code);
+
+                if (variable.IsContentVariable)
+                {
+                    // Content - Take the first value
+                    selection.ValueCodes.AddRange(GetCodes(variable, 1));
+                }
+                else if (variable.IsTime)
+                {
+                    // Time - Take the last value
+                    selection.ValueCodes.AddRange(GetTimeCodes(variable, 1));
+                }
+                else if (!variable.Elimination && (variableNumber == 1 || variableNumber == 2))
+                {
+                    // Take all values for variable 1 and 2
+                    selection.ValueCodes.AddRange(GetAllCodes(variable));
+                    variableNumber++;
+                }
+                else if (!variable.Elimination && (variableNumber > 2))
+                {
+                    // Take 1 value 
+                    selection.ValueCodes.AddRange(GetCodes(variable, 1));
+                    variableNumber++;
+                }
+                else
+                {
+                    // Select nothing for the other variables
+                }
+
+                selections.Add(selection);
+            }
+
+            return selections.ToArray();
+        }
+
+        private string[] GetCodes(Variable variable, int count)
+        {
+            var codes = variable.Values.Take(count).Select(value => value.Code).ToArray();
+
+            return codes;
+        }
+
+        private string[] GetAllCodes(Variable variable)
+        {
+            var codes = variable.Values.Take(variable.Values.Count).Select(value => value.Code).ToArray();
+
+            return codes;
+        }
+
+        private string[] GetTimeCodes(Variable variable, int count)
+        {
+            var lstCodes = variable.Values.TakeLast(count).Select(value => value.Code).ToList();
+            lstCodes.Sort((a, b) => b.CompareTo(a)); // Descending sort
+            var codes = lstCodes.ToArray();
+
+            return codes;
         }
 
         private bool HasSelection(VariablesSelection selection)
