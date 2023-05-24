231319c1969f4c9aadaf8a656288d1397d4008c4
MikaelNordberg <mikael.nordberg@scb.se>
2023-05-24 10:31:04 +0200

    BOTTOM unit tests

--- PxWeb.UnitTests/Data/DataSelectionTest.cs
+++ PxWeb.UnitTests/Data/DataSelectionTest.cs
@@ -259,6 +259,8 @@ namespace PxWeb.UnitTests.Data
             if (selection != null)
             {
                 Assert.AreEqual(10, selection.ValueCodes.Count);
+                Assert.AreEqual("0001", selection.ValueCodes[0]);
+                Assert.AreEqual("0010", selection.ValueCodes[9]);
             }
         }
 
@@ -275,10 +277,49 @@ namespace PxWeb.UnitTests.Data
             if (selection != null)
             {
                 Assert.AreEqual(5, selection.ValueCodes.Count);
+                Assert.AreEqual("0996", selection.ValueCodes[0]);
+                Assert.AreEqual("1000", selection.ValueCodes[4]);
             }
         }
 
-        
+
+        [TestMethod]
+        public void ShouldReturnBottomSelection()
+        {
+            List<string> valueCodes = new List<string>();
+
+            valueCodes.Add("bottom(10)"); // 10 values
+
+            var selections = GetSelection(valueCodes);
+
+            var selection = selections.FirstOrDefault(s => s.VariableCode == "var1");
+            if (selection != null)
+            {
+                Assert.AreEqual(10, selection.ValueCodes.Count);
+                Assert.AreEqual("0991", selection.ValueCodes[0]);
+                Assert.AreEqual("1000", selection.ValueCodes[9]);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnBottomOffsetSelection()
+        {
+            List<string> valueCodes = new List<string>();
+
+            valueCodes.Add("bottom(10,995)"); // 5 values
+
+            var selections = GetSelection(valueCodes);
+
+            var selection = selections.FirstOrDefault(s => s.VariableCode == "var1");
+            if (selection != null)
+            {
+                Assert.AreEqual(5, selection.ValueCodes.Count);
+                Assert.AreEqual("0001", selection.ValueCodes[0]);
+                Assert.AreEqual("0005", selection.ValueCodes[4]);
+            }
+        }
+
+
         // Helper methods
 
 
