4555e49b511e0d2502bf115320a4eb6b548fba04
Jonathan Erikson <jonatan.erikson@gmail.com>
2023-06-15 09:41:05 +0200

    Add classes for saving state of async tasks

---
+++ PXWeb/Code/BackgroundWorker/ResponseState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PxWeb.Code.BackgroundWorker
+{
+    public class ResponseState
+    {
+        public string State { get; set; } = "Not run";
+        public DateTime? LastRun { get; set; }
+        public List<Event> EventsFromLastRun { get; set; } = new List<Event>();
+
+        public void AddEvent(Event e)
+        {
+            EventsFromLastRun.Add(e);
+        }
+        public void Begin()
+        {
+            EventsFromLastRun = new List<Event>();
+            AddEvent(new Event("Information", $"Started at {DateTime.UtcNow}"));
+            State = "Running";
+        }
+        public void End()
+        {
+            LastRun = DateTime.UtcNow;
+            AddEvent(new Event("Information", $"Finished at {LastRun}"));
+            State = "Finished";
+        }
+    }
+
+    public class Event
+    {
+        public string Type { get; set; }
+        public string Message { get; set; }
+
+        public Event(string type, string message)
+        {
+            Type = type;
+            Message = message;
+        }
+    }
+}
