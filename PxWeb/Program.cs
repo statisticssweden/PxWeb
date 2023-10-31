8542161e5d6616656db98a89140d248b740a3e38
oyessb <47382101+oyessb@users.noreply.github.com>
2023-10-31 15:18:43 +0100

    Test swagger routeprefix to empty

--- PxWeb/Program.cs
+++ PxWeb/Program.cs
@@ -137,7 +137,12 @@ namespace PxWeb
             //if (app.Environment.IsDevelopment())
             //{
             app.UseSwagger();
-            app.UseSwaggerUI();
+            //app.UseSwaggerUI();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("./swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
             //}
             app.UseHttpsRedirection();
 
