0873e612f94a0ebceb616e7ad18e896ffa5ea83d
likp <petros.likidis@scb.se>
2024-05-28 20:59:30 +0200

    Added databases specific folder for bulk files.

--- PXWeb/Code/API/Controllers/BulkController.cs
+++ PXWeb/Code/API/Controllers/BulkController.cs
@@ -11,6 +11,7 @@ using PXWeb.Management;
 using PCAxis.Menu;
 using PCAxis.Paxiom.Localization;
 using PCAxis.Sql.DbConfig;
+using System.IO;
 
 namespace PXWeb.Code.API.Controllers
 {
@@ -21,10 +22,48 @@ namespace PXWeb.Code.API.Controllers
         public HttpResponseMessage CreateBulkFiles(string database, string language)
         {
             var tables = GetTables(database, language);
+            string bulkRoot = GetBulkRoot();
 
-
+            var dbPath = System.IO.Path.Combine(bulkRoot, database);
+            var tempPath = System.IO.Path.Combine(dbPath, "temp");
+            InitDatabaseFolder(dbPath, tempPath);
 
             return Request.CreateResponse(HttpStatusCode.OK, $"Bulk files created successfully for database {database}");
+        }
+
+        private static string GetBulkRoot()
+        {
+            var path = System.Web.HttpContext.Current.Server.MapPath(Settings.Current.General.Paths.PxDatabasesPath);
+            if (path.EndsWith("\\"))
+            {
+                path = System.IO.Path.GetDirectoryName(path);
+            }
+            var bulkRoot = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), "bulk");
+            return bulkRoot;
+        }
+
+        private static void InitDatabaseFolder(string dbPath, string tempPath)
+        {
+            if (!System.IO.Directory.Exists(dbPath))
+            {
+                Directory.CreateDirectory(dbPath);
+                Directory.CreateDirectory(tempPath);
+            }
+            else
+            {
+                if (!System.IO.Directory.Exists(tempPath))
+                {
+                    Directory.CreateDirectory(tempPath);
+                }
+                {
+                    // Clear temp folder    
+                    var tempFiles = Directory.GetFiles(tempPath);
+                    foreach (var file in tempFiles)
+                    {
+                        System.IO.File.Delete(file);
+                    }
+                }
+            }
         }
 
         static List<TableLink> GetTables(string database, string language)
@@ -35,7 +74,7 @@ namespace PXWeb.Code.API.Controllers
 
             return tables;
         }
-
+        
         static void AddTables(PCAxis.Menu.Item item, List<TableLink> tables)
         {
             if (item is PxMenuItem)
