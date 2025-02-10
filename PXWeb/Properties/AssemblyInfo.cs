f54af3b88a487ce909132ce01462ecef930b5c85
veronica.bragden <veronica.bragden@scb.se>
2025-02-10 11:53:14 +0100

    Changed version number for new release

--- PXWeb/Properties/AssemblyInfo.cs
+++ PXWeb/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SCB")]
 [assembly: AssemblyProduct("PXWeb")]
-[assembly: AssemblyCopyright("Copyright © SCB 2023")]
+[assembly: AssemblyCopyright("Copyright © SCB 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("23.0.0.*")]
-[assembly: AssemblyFileVersion("23.0.0.0")]
+[assembly: AssemblyVersion("25.1.0.*")]
+[assembly: AssemblyFileVersion("25.1.0.0")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
